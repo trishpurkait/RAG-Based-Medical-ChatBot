2ed47d6847932a9fadcc0e87d5f5b2c4ae761928
trishpurkait <trishpurkait@gmail.com>
2026-05-13 14:05:59 +0530

    file structure is added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='rag_medical_chatbot',
+    version='0.0.0',
+    author='TRISH',
+    author_email='trishpurkat@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
