09d94dbf70dbf1b42306929cf52f8094d853989b
trishpurkait <trishpurkait@gmail.com>
2026-05-17 11:05:04 +0530

    "added exception"

--- src/llm.py
+++ src/llm.py
@@ -1,9 +1,12 @@
 from langchain_openai import ChatOpenAI
 from src.config import *
 
-llm = ChatOpenAI(
-    model=MODEL_NAME,
-    base_url="https://integrate.api.nvidia.com/v1",
-    api_key=NVIDIA_NIM,
-    temperature=0.2
-)
+try:
+    llm = ChatOpenAI(
+        model=MODEL_NAME,
+        base_url="https://integrate.api.nvidia.com/v1",
+        api_key=NVIDIA_NIM,
+        temperature=0.2
+    )
+except Exception as e:
+    print("Error initializing LLM:", e)
